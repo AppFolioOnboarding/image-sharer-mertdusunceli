fb83f6681f09eb044e235b7390c725f80237e216
Mert Dusunceli <mert.dusunceli@appfolio.com>
2020-02-11 17:29:49 -0800

    Add image view and controller

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,29 @@
+class ImagesController < ApplicationController
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to @image
+    else
+      render 'new'
+    end
+    # render plain: params[:article].inspect
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  def index
+    @images = Image.all
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:image_url)
+  end
+end
