8baad1f2a89d1bd56c8b8392d1565d887ada7bd1
Mert Dusunceli <mert.dusunceli@appfolio.com>
2020-02-12 16:35:23 -0800

    Add 20 seed images to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+images = Image.create([
+{image_url: 'https://upload.wikimedia.org/wikipedia/commons/4/49/Koala_climbing_tree.jpg'},
+{image_url: 'https://static01.nyt.com/images/2017/08/01/science/29TB-PLATYPUS1/29TB-PLATYPUS1-superJumbo.jpg'},
+{image_url: 'https://i.dailymail.co.uk/i/pix/2017/05/30/14/40EDF72700000578-4555280-image-a-27_1496151358954.jpg'},
+{image_url: 'http://media.trevorsbirding.com/wp-content/uploads/2009/12/img_8609-1024x768.jpg'},
+{image_url: 'https://www.openforum.com.au/wp-content/uploads/2019/09/tasmanian-devil-pixabay-610x400.jpg'},
+{image_url: 'https://www.australiangeographic.com.au/wp-content/uploads/2018/11/wombat-australia.jpg'},
+{image_url: 'https://static.scientificamerican.com/sciam/cache/file/A15A3D54-028D-48D0-80B602C6F9348F8C_source.jpg'},
+{image_url: 'https://reptilepark.com.au/wp-content/uploads/2015/12/crocodiles_saltwater2-1030x542.jpg'},
+{image_url: 'https://www.mascotarios.org/wp-content/uploads/2014/11/Cacatua-Galerita.jpg'},
+{image_url: 'https://blogs.biomedcentral.com/on-biology/wp-content/uploads/sites/5/2019/02/Dingosblog-620x342.jpg'},
+{image_url: 'https://img.theculturetrip.com/768x432/wp-content/uploads/2017/11/shutterstock_174055787.jpg'},
+{image_url: 'https://www.australiascoralcoast.com/sites/cc/files/styles/full_width_slider/public/2018-10/Sea%20Lion%20Jumbo.jpg'},
+{image_url: 'https://www.sciencemag.org/sites/default/files/styles/article_main_image_-_1280w__no_aspect_/public/shark_1280p.jpg'},
+{image_url: 'https://oceana.org/sites/default/files/styles/lightbox_full/public/shutterstock_bernd.neeser_dugong.jpg'},
+{image_url: 'https://www.mediastorehouse.com/p/632/brown-fur-seal-arctocephalus-pusillus-15423640.jpg'},
+{image_url: 'https://upload.wikimedia.org/wikipedia/commons/6/61/Humpback_Whale_underwater_shot.jpg'},
+{image_url: 'https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/jelly_16x9.jpg'},
+{image_url: 'https://compote.slate.com/images/086f1f29-1164-4f93-a25c-8ed7de492311.jpg'},
+{image_url: 'https://thenypost.files.wordpress.com/2018/11/istock-184948807.jpg'},
+{image_url: 'http://natgeo.imgix.net/factsheets/thumbnails/Dolphin22.jpg'}])
