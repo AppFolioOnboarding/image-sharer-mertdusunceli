0d796c13b7668a6a94040852d9e23cc33de7955a
Mert Dusunceli <mert.dusunceli@appfolio.com>
2020-02-20 16:52:31 -0800

    md - Add Footer and Form components and tests

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './Header';
+import Form from './Form';
+import Footer from './Footer';
 
 export default function App() {
   return (
     <div>
       <Header title="Tell us what you think" />
-      Put your components here: Flash Message, Form, Footer
+      <Form />
+      <Footer />
     </div>
   );
 }
